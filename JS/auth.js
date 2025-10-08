64546ecc97e06667135659b591a7048c0a9ead31
Daniel Campos <danielcampos.santos@ucsal.edu.br>
2025-10-08 16:13:41 -0300

    Correção de erros de commits e adição da pagina de login no main

---
+++ JS/auth.js
@@ -0,0 +1,23 @@
+
+const loginContainer = document.getElementById('login');
+const emailInput = document.getElementById('email');
+const senhaInput = document.getElementById('senha');
+
+function entrar() {
+    // Remove qualquer mensagem existente
+    const mensagemExistente = loginContainer.querySelector('.vazio');
+    if (mensagemExistente) {
+        mensagemExistente.remove();
+    }
+    
+    if (emailInput.value && senhaInput.value) {
+        window.location.href = '#';
+    } else {
+        const mensagemSpan = document.createElement('span');
+        mensagemSpan.textContent = 'Insira o email e a senha para entrar';
+        mensagemSpan.classList.add('vazio');
+        
+        loginContainer.insertBefore(mensagemSpan,document.getElementById('entrar'))
+    }
+}
+
