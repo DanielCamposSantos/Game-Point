1819df5f6d70bf99c741548188ea1956bf6bc13b
Daniel Campos <danielcampos.santos@ucsal.edu.br>
2025-11-10 14:49:41 -0300

    atualização no sistema de avaliação

--- JS/avaliacao.js
+++ JS/avaliacao.js
@@ -1,30 +1,30 @@
 const avaliacoes = document.getElementsByClassName('avaliacao');
 
 for (let i = 0; i < avaliacoes.length; i++) {
-    const stars = '☆☆☆☆☆'.split('');
+    const estrelas = '☆☆☆☆☆'.split('');
     avaliacoes[i].textContent = ''; 
     
     
-    stars.forEach((star, index) => {
-        const starSpan = document.createElement('span');
-        starSpan.textContent = star;
-        starSpan.classList.add('estrelas');
+    estrelas.forEach((star, index) => {
+        const estrelasSpan = document.createElement('span');
+        estrelasSpan.textContent = star;
+        estrelasSpan.classList.add('estrelas');
         
-        starSpan.addEventListener('click', () => {
-            const starsInDiv = avaliacoes[i].getElementsByClassName('estrelas');
-            for (let j = 0; j < starsInDiv.length; j++) {
+        estrelasSpan.addEventListener('click', () => {
+            const estrelasnaDiv = avaliacoes[i].getElementsByClassName('estrelas');
+            for (let j = 0; j < estrelasnaDiv.length; j++) {
                 if (j <= index) {
-                    starsInDiv[j].textContent = '★';
-                    starsInDiv[j].classList.add('full');
-                    starsInDiv[j].classList.remove('empty');
+                    estrelasnaDiv[j].textContent = '★';
+                    estrelasnaDiv[j].classList.add('full');
+                    estrelasnaDiv[j].classList.remove('empty');
                 } else {
-                    starsInDiv[j].textContent = '☆';
-                    starsInDiv[j].classList.add('empty');
-                    starsInDiv[j].classList.remove('full');
+                    estrelasnaDiv[j].textContent = '☆';
+                    estrelasnaDiv[j].classList.add('empty');
+                    estrelasnaDiv[j].classList.remove('full');
                 }
             }
         });
         
-        avaliacoes[i].appendChild(starSpan);
+        avaliacoes[i].appendChild(estrelasSpan);
     })
 }
